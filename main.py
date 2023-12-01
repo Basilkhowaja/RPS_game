55aaf781bf04d19673dce3acebf942c73db1e734
bk08432 <bk08432@st.habib.edu.pk>
2023-12-01 10:34:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pygame
 pygame.init()
 
 
-#loading all game images
+#loading all game images, including match result images:
+
 background=pygame.image.load('bg.jpg')
 rock=pygame.image.load('rock.png')
 paper=pygame.image.load('paper.jpg')
@@ -14,21 +15,27 @@ choose=pygame.image.load('choose.jpg')
 win=pygame.image.load('win.jpg')
 tie=pygame.image.load('tie.jpg')
 again=pygame.image.load('again.jpg')
+rr=pygame.image.load("rr.jpg")
+rp=pygame.image.load("rp.jpg")
+rs=pygame.image.load("rs.jpg")
+pr=pygame.image.load("pr.jpg")
+pp=pygame.image.load("pp.jpg")
+ps=pygame.image.load("ps.jpg")
+sr=pygame.image.load("sr.jpg")
+sp=pygame.image.load("sp.jpg")
+ss=pygame.image.load("ss.jpg")
 
 
-s=['rock','paper','scissor']
-#c=random.choice(s)
 
-#user=input("enter your choice")
 gameDisplay=pygame.display.set_mode((800,700))
 
 gameDisplay.blit(background,(0,0))
 pygame.display.update()
 sleep(3)
 
-c='paper'
 
-#make use of function here
+
+#making use of function here
 
 def game():
     run=True
@@ -42,44 +49,49 @@ def game():
             elif event.type==pygame.KEYDOWN:
                 key=pygame.key.get_pressed()
                 run=False
-    
-    if key[pygame.K_r]==True and c=='rock':
-        gameDisplay.blit(tie,(0,0)) 
-        pygame.display.update()
-        sleep(2)
 
-    elif key[pygame.K_r]==True and c=='paper':
-        gameDisplay.blit(lost,(0,0))
+    #computer choice through random() built_in function, it will select through the list s.            
+    s=['rock','paper','scissor']   #selecting the computer choice inside the game function so that whenever user wants to play again, the new value of C gets selected.
+    computer=random.choice(s)
+
+#giving conditions now on which choice to display which image, we designed these images through canva.com
+
+    if key[pygame.K_r]==True and computer=='rock':
+        gameDisplay.blit(rr,(0,0)) 
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_r]==True and c=='scissor':
-        gameDisplay.blit(win,(0,0))
+        sleep(5)
+    elif key[pygame.K_r]==True and computer=='paper':
+        gameDisplay.blit(rp,(0,0))
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_p]==True and c=='rock':
-        gameDisplay.blit(win,(0,0))
+        sleep(5)
+    elif key[pygame.K_r]==True and computer=='scissor':
+        gameDisplay.blit(rs,(0,0))
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_p]==True and c=='paper':
-        gameDisplay.blit(tie,(0,0))
+        sleep(5)
+    elif key[pygame.K_p]==True and computer=='rock':
+        gameDisplay.blit(pr,(0,0))
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_p]==True and c=='scissor':
-        gameDisplay.blit(lost,(0,0))
+        sleep(5)
+    elif key[pygame.K_p]==True and computer=='paper':
+        gameDisplay.blit(pp,(0,0))
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_s]==True and c=='rock':
-        gameDisplay.blit(lost,(0,0))
+        sleep(5)
+    elif key[pygame.K_p]==True and computer=='scissor':
+        gameDisplay.blit(ps,(0,0))
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_s]==True and c=='paper':
-        gameDisplay.blit(win,(0,0))
+        sleep(5)
+    elif key[pygame.K_s]==True and computer=='rock':
+        gameDisplay.blit(sr,(0,0))
         pygame.display.update()
-        sleep(2)
-    elif key[pygame.K_s]==True and c=='scissor':
-        gameDisplay.blit(tie,(0,0))
+        sleep(5)
+    elif key[pygame.K_s]==True and computer=='paper':
+        gameDisplay.blit(sp,(0,0))
         pygame.display.update()
-        sleep(2)
+        sleep(5)
+    elif key[pygame.K_s]==True and computer=='scissor':
+        gameDisplay.blit(ss,(0,0))
+        pygame.display.update()
+        sleep(5)
     run=True
     key=None
     while run:
